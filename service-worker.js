d9359a9163810024ac88938c26d1656cf0663c60
KolevPld <kmet.zapaden@gmail.com>
2026-01-02 18:33:16 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,15 @@
-const CACHE_NAME = 'calculator-cache-v1';
+const CACHE_NAME = 'calculator-cache-v2';
+
 const FILES_TO_CACHE = [
   './index.html',
   './manifest.json',
   './icon-192.png',
-  './icon-512.png'
+  './icon-512.png',
+  './styles.css',     // ако имаш отделен CSS файл
+  './script.js'       // ако JS е изнесен
 ];
 
+// Кеширане при инсталиране
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -14,6 +18,19 @@ self.addEventListener('install', event => {
   );
 });
 
+// Изчистване на стари кешове
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Отговаря със съдържание от кеша или прави fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
