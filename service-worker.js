c084e54c1595e6fa015ff1027583aeca8a279fb6
KolevPld <kmet.zapaden@gmail.com>
2026-01-02 18:44:18 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,29 @@
 const CACHE_NAME = 'calculator-cache-v2';
-
 const FILES_TO_CACHE = [
+  './',
   './index.html',
   './manifest.json',
   './icon-192.png',
-  './icon-512.png',
-  './styles.css',     // ако имаш отделен CSS файл
-  './script.js'       // ако JS е изнесен
+  './icon-512.png'
 ];
 
-// Кеширане при инсталиране
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-// Изчистване на стари кешове
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      );
-    })
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
   );
 });
 
-// Отговаря със съдържание от кеша или прави fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
